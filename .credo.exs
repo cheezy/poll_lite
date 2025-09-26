21703a3b0ab58cdc78fd83d11f1770fd814e2a24
Jeffrey S. Morgan <cheezy@industriallogic.com>
2025-09-26 07:33:29 -0400

    updated credo rules

--- .credo.exs
+++ .credo.exs
@@ -7,7 +7,9 @@
         # Additional and reconfigured checks
         {Credo.Check.Design.AliasUsage, if_nested_deeper_than: 3, if_called_more_often_than: 1},
         {Credo.Check.Readability.AliasAs, []},
-        {Credo.Check.Readability.Specs, []},
+        {Credo.Check.Readability.MultiAlias, []},
+        {Credo.Check.Readability.NestedFunctionCalls, files: %{excluded: ["lib/pool_lite/polls.ex"]}},
+        {Credo.Check.Readability.SeparateAliasRequire, []},
         {Credo.Check.Readability.StrictModuleLayout, []},
         {Credo.Check.Readability.WithCustomTaggedTuple, []},
         {Credo.Check.Warning.UnsafeToAtom, []},
@@ -16,8 +18,6 @@
         {Credo.Check.Design.TagFIXME, false},
         {Credo.Check.Design.TagTODO, false},
         {Credo.Check.Readability.ModuleDoc, false},
-        {Credo.Check.Readability.ImplTrue, false},
-        {Credo.Check.Readability.PredicateFunctionNames, false},
         {Credo.Check.Refactor.LongQuoteBlocks, false},
         {Credo.Check.Refactor.Nesting, false}
       ]
